806504fe8247589ba2bc3af8179267e5b6966195
DanielGuachamin <guillermo.guachamin@epn.edu.ec>
2022-07-28 23:29:40 -0500

    plantilla 3 cv

---
+++ App_Grupal_HojadeVida/App_Grupal_HojadeVida/Elemento.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App_Grupal_HojadeVida
+{
+    public class Elemento
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+
+    public class ListaElementos
+    {
+        public List<Elemento> _elementos { get; set; }
+
+        public ListaElementos()
+        {
+            _elementos = new List<Elemento>();
+            loadElements();
+        }
+
+        public void loadElements()
+        {
+            _elementos.Add(new Elemento
+            {
+                Nombre = "Java",
+                Descripcion = "Se manejar Java"
+            });
+            _elementos.Add(new Elemento
+            {
+                Nombre = "C++",
+                Descripcion = "Se manejar C++"
+            });
+            _elementos.Add(new Elemento
+            {
+                Nombre = "Python",
+                Descripcion = "Se manejar Python"
+            });
+        }
+    }
+}
